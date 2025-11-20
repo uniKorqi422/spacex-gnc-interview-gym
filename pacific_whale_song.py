dc3697af41dbb1845a57612798bb52cb9895b69f
Amanda Meehan <rocketpy.tumbler@proton.me>
2025-11-20 03:14:29 -0800

    Corrected last name

--- pacific_whale_song.py
+++ pacific_whale_song.py
@@ -2,6 +2,6 @@
 PacificWhaleSong
 A Starlink v1 sprite on its final journey home to the Pacific
 Because every satellite deserves a dignified goodbye
-Author: Amanda Wech (@Am1Alpha)
+Author: Amanda Wech-Meehan (@Am1Alpha)
 Date: 2025-11-20
 """
