8adbc1355882b642efed7a39ae55f75ab630e765
Amanda Meehan <rocketpy.tumbler@proton.me>
2025-11-25 04:13:32 -0800

    Testing `pymsis` with `density`

---
+++ test_pymsis.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import pymsis
+import numpy as np
+
+
+density = pymsis.msis00f.msis00f(
+    altitude=250.0,
+    longitude=-140.0,
+    latitude=0.0,
+    f107=150.0,
+    f107a=150.0,
+    ap=15.0,
+    date=datetime(2025, 11, 25)
+)
+print(f"Density at 250 km: {density[0, 0]:.3e} kg/m³")
